3a7d9add86e65e52f6b1cdd58dc6c272a4bcc343
Marco Mendoza <mmi.mendoza@gmail.com>
2021-01-12 09:34:51 -0500

    cambio0001

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:contrador/src/app.dart';
+import 'package:flutter/material.dart';
+
+//import 'src/app.dart';
+
+void main() {
+  runApp(new MyApp());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:contrador/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: HomePage(),
+      ),
+    );
+  }
+}
